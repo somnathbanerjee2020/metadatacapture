fafe82bd0eae9f1436a4f9e27090835e9709fd7f
Somnath Banerjee <35511147+somnathbanerjee2020@users.noreply.github.com>
2021-08-27 16:49:56 +1000

    tables added

---
+++ Tables/metadata.job_run_status.sql
@@ -0,0 +1,10 @@
+create table metadata.job_run_status
+(
+	run_id             INT NOT NULL AUTO_INCREMENT,
+	job_name           varchar(100),
+	start_time         timestamp,
+	end_time           timestamp,
+	status             varchar(100),
+	records_inserted   int,
+	primary key(run_id)
+);
